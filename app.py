c2c8a07d36835e35dae9a17dbb481f21a3f43219
hiren1204 <47883219+hiren1204@users.noreply.github.com>
2021-01-04 19:21:48 +0530

    Update app.py
    get and add doctor (sample api)

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
+doctor_list = []
 
-
-db = []
+class Doctor(BaseModel):
+    name: str
+    type: str
 
 @app.get('/doctor')
-def index():
-    return {'key' : 'value'}
+def get_doctor():
+    return doctor_list
 
+@app.get('/doctor/{doctor_id}')
+def get_doctor_via_id(doctor_id: int):
+    return doctor_list[doctor_id-1]
 
 @app.post('/doctor')
+def add_doctor(doctor: Doctor):
+    doctor_list.append(doctor.dict())
+    return doctor_list[-1]
 
-
-
-
-
+@app.delete('/doctor/{doctor_id}')
+def delete_doctor_via_id(doctor_id: int):
+    doctor_list.pop(doctor_id-1)
+    return {}
 
 uvicorn.run(app)
