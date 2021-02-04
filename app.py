1e52d0a2a5300c90abb9263b5cb003371726586b
Keval <keval.bda1716@ict.gnu.ac.in>
2021-02-04 23:16:23 +0530

    Signup Page Created

--- app.py
+++ app.py
@@ -79,6 +79,9 @@ async def home(request: Request):
 async def home(request: Request):
     return templates.TemplateResponse("signup.html", {"request": request})
 
+@app.get('/login', response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
 
 @app.get('/doctor')
@@ -103,4 +106,4 @@ def delete_doctor_via_id(doc_id: int):
     return {}
 
 
-#uvicorn.run(app)
+uvicorn.run(app)
