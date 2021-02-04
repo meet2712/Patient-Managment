7be0eca45afdb5b153c7642b8e66dd6f27ab9f7c
hiren1204 <47883219+hiren1204@users.noreply.github.com>
2021-02-04 15:48:32 +0530

    Changes in Database(HMS)
    new table (hospital)  has been added to heroku databse

--- hms.sql
+++ hms.sql
@@ -1,98 +1,110 @@
--- phpMyAdmin SQL Dump
--- version 4.7.9
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.5.62, for Linux (x86_64)
 --
--- Host: 127.0.0.1:3302
--- Generation Time: Jan 06, 2021 at 06:24 AM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: heroku_cb8e53992ffbeaf
+-- ------------------------------------------------------
+-- Server version	5.5.62-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hms`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `doctor`
 --
 
 DROP TABLE IF EXISTS `doctor`;
-CREATE TABLE IF NOT EXISTS `doctor` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doctor` (
   `doc_id` int(11) NOT NULL,
   `doc_name` varchar(20) NOT NULL,
   `doc_type` varchar(30) NOT NULL,
-  `doc_ph_no` int(11) NOT NULL,
+  `doc_ph_no` bigint(11) NOT NULL,
   `doc_email` varchar(30) NOT NULL,
   PRIMARY KEY (`doc_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `doctor`
 --
 
-INSERT INTO `doctor` (`doc_id`, `doc_name`, `doc_type`, `doc_ph_no`, `doc_email`) VALUES
-(1, 'suresh', 'dentist', 909878675, ''),
-(2, 'krishna iyer', 'cardiologist', 923644765, '');
-
--- --------------------------------------------------------
+LOCK TABLES `doctor` WRITE;
+/*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
+INSERT INTO `doctor` VALUES (1,'Suresh','Dentist',8141276872,''),(2,'Krishna Iyer','Cardiologist',9409299016,''),(3,'Meet Vaghasia','Surgeon',7990688061,'meet.bda1729@ict.gnu.ac.in');
+/*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `patient`
 --
 
 DROP TABLE IF EXISTS `patient`;
-CREATE TABLE IF NOT EXISTS `patient` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient` (
   `p_id` int(10) NOT NULL,
   `p_name` varchar(20) NOT NULL,
-  `p_ph_no` int(11) NOT NULL,
+  `p_ph_no` bigint(11) NOT NULL,
   `report_id` int(10) NOT NULL,
   PRIMARY KEY (`p_id`),
   KEY `report_id` (`report_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `patient`
 --
 
-INSERT INTO `patient` (`p_id`, `p_name`, `p_ph_no`, `report_id`) VALUES
-(1, 'hiren koradiya', 987654321, 1),
-(2, 'dixit', 987876567, 2);
-
--- --------------------------------------------------------
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+INSERT INTO `patient` VALUES (1,'hiren koradiya',9409299016,1),(2,'dixit',987876567,2);
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `reports`
 --
 
 DROP TABLE IF EXISTS `reports`;
-CREATE TABLE IF NOT EXISTS `reports` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reports` (
   `report_id` int(10) NOT NULL,
   `report_type` varchar(30) NOT NULL,
   `p_id` int(10) NOT NULL,
   UNIQUE KEY `p_id` (`p_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `reports`
 --
 
-INSERT INTO `reports` (`report_id`, `report_type`, `p_id`) VALUES
-(1, 'urin test', 1),
-(2, 'blood test', 2);
-COMMIT;
+LOCK TABLES `reports` WRITE;
+/*!40000 ALTER TABLE `reports` DISABLE KEYS */;
+INSERT INTO `reports` VALUES (1,'urin test',1),(2,'blood test',2);
+/*!40000 ALTER TABLE `reports` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping routines for database 'heroku_cb8e53992ffbeaf'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-04  5:58:36
