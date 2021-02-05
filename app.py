67260996fe45a7f0cca2dc4c3df73edd9fbbad34
Meet <meet.bda1729@ict.gnu.ac.in>
2021-02-05 13:18:20 +0530

    Cursor Error Fixed

--- app.py
+++ app.py
@@ -20,9 +20,10 @@ mycursor = mydb.cursor()
 mycursor.execute("use heroku_cb8e53992ffbeaf")
 mycursor.execute("select * from doctor")
 
+
 for db in mycursor:
     print(db)
-
+mydb.commit()
 
 app = FastAPI(template_folder='Templates/')
 templates = Jinja2Templates(directory="Templates/")
@@ -82,48 +83,51 @@ async def home(request: Request):
 @app.get('/doctor')
 def get_doc():
     # return doctor_list
-    # mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
     mycursor.execute("use heroku_cb8e53992ffbeaf")
     mycursor.execute("select * from doctor")
     doctor_list = []
     for x in mycursor:
         doctor_list.append(x)
+    mydb.commit()
     return doctor_list
 
 
 @app.get('/patient')
 def get_doc():
     # return patient_list
-    #mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
     mycursor.execute("use heroku_cb8e53992ffbeaf")
     mycursor.execute("select * from patient")
     patient_list = []
     for x in mycursor:
         patient_list.append(x)
+    mydb.commit()
     return patient_list
 
 
 @app.get('/hospital')
 def get_doc():
     # return hospital_list
-    #mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
     mycursor.execute("use heroku_cb8e53992ffbeaf")
     mycursor.execute("select * from hospital")
     hospital_list = []
     for x in mycursor:
         hospital_list.append(x)
+    mydb.commit()
     return hospital_list
 
 
 @app.get('/reports')
 def get_doc():
-    # return reports_list
-    #mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
     mycursor.execute("use heroku_cb8e53992ffbeaf")
     mycursor.execute("select * from reports")
     reports_list = []
     for x in mycursor:
         reports_list.append(x)
+    mydb.commit()
     return reports_list
 
 
@@ -143,4 +147,5 @@ def delete_doctor_via_id(doc_id: int):
     doctor_list.pop(doc_id-1)
     return {}
 
-# uvicorn.run(app)
+
+uvicorn.run(app)
