a400f03b4fd6403058e77723219746f4285d74b1
Meet <meet.bda1729@ict.gnu.ac.in>
2021-03-05 16:26:45 +0530

    Appointment BASIC API Done with JSON Format

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get('/test', response_class=HTMLResponse)
-async def test(request: Request):
-    return templates.TemplateResponse("test.html", {"request": request})
+# @app.get('/test', response_class=HTMLResponse)
+# async def test(request: Request):
+#     return templates.TemplateResponse("test.html", {"request": request})
 
 
 @app.get('/signup', response_class=HTMLResponse)
@@ -43,53 +43,6 @@ async def login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 
-@app.get('/index2', response_class=HTMLResponse)
-async def index2(request: Request):
-    return templates.TemplateResponse("index2.html", {"request": request})
-
-
-
-
-# @app.get('/doctor/{doctor_id}')
-# def get_doctor_via_id(doc_id: int):
-#     return doctor_list[doc_id - 1]
-#
-#
-# @app.post('/doctor')
-# def add_doctor(doctor: Doctor):
-#     doctor_list.append(doctor.dict())
-#     return doctor_list[-1]
-#
-#
-# @app.delete('/doctor/{doctor_id}')
-# def delete_doctor_via_id(doc_id: int):
-#     doctor_list.pop(doc_id - 1)
-#     return {}
-
-
-# from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-# from fastapi import Depends
-#
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
-
-
-# @app.post('/token')
-# async def token(form_data: OAuth2PasswordRequestForm = Depends()):
-#     return {'access_token': form_data.username + 'token'}
-
-############################## This can be used ##########################
-# @app.post('/token')
-# async def token(username, password):
-#     return {'access_token': username + password + 'token'}
-
-
-#
-# @app.get('/')
-# async def index(token: str = Depends(oauth2_scheme)):
-#     return {'the_token' : token}
-
-
-# app = FastAPI()
 
 JWT_SECRET = 'myjwtsecret'
 
@@ -156,7 +109,7 @@ async def get_user(user: User_Pydantic = Depends(get_current_user)):
 
 
 @app.get('/patient')
-async def get_doc(user: User_Pydantic = Depends(get_current_user)):
+async def get_patient(user: User_Pydantic = Depends(get_current_user)):
     mydb = mysql.connector.connect(host="us-cdbr-east-03.cleardb.com", user="b4b07506295099", passwd="90df5ad7")
     mycursor = mydb.cursor()
     mycursor.execute("use heroku_cb8e53992ffbeaf")
@@ -187,7 +140,7 @@ def get_doc(user: User_Pydantic = Depends(get_current_user)):
 
 
 @app.get('/hospital')
-def get_doc(user: User_Pydantic = Depends(get_current_user)):
+def get_hospital(user: User_Pydantic = Depends(get_current_user)):
     mydb = mysql.connector.connect(host="us-cdbr-east-03.cleardb.com", user="b4b07506295099", passwd="90df5ad7")
     mycursor = mydb.cursor()
     mycursor.execute("use heroku_cb8e53992ffbeaf")
@@ -202,7 +155,7 @@ def get_doc(user: User_Pydantic = Depends(get_current_user)):
 
 
 @app.get('/reports')
-def get_doc(user: User_Pydantic = Depends(get_current_user)):
+def get_report(user: User_Pydantic = Depends(get_current_user)):
     mydb = mysql.connector.connect(host="us-cdbr-east-03.cleardb.com", user="b4b07506295099", passwd="90df5ad7")
     mycursor = mydb.cursor()
     mycursor.execute("use heroku_cb8e53992ffbeaf")
@@ -217,11 +170,11 @@ def get_doc(user: User_Pydantic = Depends(get_current_user)):
 
 
 @app.get('/schedule/{doctor_id}')
-def schedule(doctor_id, user: User_Pydantic = Depends(get_current_user)):
+def get_schedule_doc(doctor_id, user: User_Pydantic = Depends(get_current_user)):
     mydb = mysql.connector.connect(host="us-cdbr-east-03.cleardb.com", user="b4b07506295099", passwd="90df5ad7")
     mycursor = mydb.cursor()
     tuple1 = (doctor_id,)
-    query = """ select * from schedule where doc_id = %s """
+    query = """ select schedule_id, doc_id, cast(date as CHAR) as date, cast(time as CHAR) as time, status from schedule where doc_id = %s """
     mycursor.execute("use heroku_cb8e53992ffbeaf")
     mycursor.execute(query, tuple1)
     schedule_list = mycursor.fetchall()
@@ -234,11 +187,11 @@ def schedule(doctor_id, user: User_Pydantic = Depends(get_current_user)):
 
 
 @app.get('/schedule/{doctor_id}/{date}')
-def schedule(doctor_id, date, user: User_Pydantic = Depends(get_current_user)):
+def get_schedule_doc_date(doctor_id, date, user: User_Pydantic = Depends(get_current_user)):
     mydb = mysql.connector.connect(host="us-cdbr-east-03.cleardb.com", user="b4b07506295099", passwd="90df5ad7")
     mycursor = mydb.cursor()
     tuple1 = (doctor_id, date)
-    query = """ select schedule_id, doc_id, cast(date as CHAR) as date1, cast(time as CHAR) as time1, status from schedule where doc_id = %s and date = %s """
+    query = """ select schedule_id, doc_id, cast(date as CHAR) as date, cast(time as CHAR) as time, status from schedule where doc_id = %s and date = %s """
     mycursor.execute("use heroku_cb8e53992ffbeaf")
     mycursor.execute(query, tuple1)
     schedule_list = mycursor.fetchall()
