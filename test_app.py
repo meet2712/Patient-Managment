b12f1ffbe49658993973a8528663428ef6273fe7
Meet Vaghasia <meet.bda1729@ict.gnu.ac.in>
2021-03-31 12:08:06 +0530

    Trial TestCases

--- test_app.py
+++ test_app.py
@@ -26,12 +26,13 @@ def test_doctor_app():
         'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NCwibmFtZSI6Im1lZXQiLCJ1c2VybmFtZSI6Im1lZXQyNzEyIiwiZW1haWwiOiJtZWV0LmJkYTE3MjlAaWN0LmdudS5hYy5pbiIsInVzZXJ0eXBlIjoiYWRtaW4iLCJwYXNzd29yZF9oYXNoIjoiJDJiJDEyJDNLZHpXZHF6RUN6TlU5RmRmWmVwM3VIcmhXajUuU3dpdjFNaHVRTWlMcVJaTFJHcUVaMnVDIn0.gKzJs32Z-a3SS-k5qZ-hVyhiyKoWXQKauyXHRojDzG8'
     }
 
-   # response = pytest_python_requests.request("GET", url, headers=headers, data=payload)
-    response =  pytest_python_requests.format_response("GET", url, headers=headers, data=payload)
+    response = pytest_python_requests.request("GET", url, headers=headers, data=payload)
+    #response =  pytest_python_requests.format_response("GET", url, headers=headers, data=payload)
 
     print(response.status_code)
     assert response.status_code == 200
     assert response.json() == [{"hospital_id": 4,"hospital_name": "Cims"},{ "hospital_id": 5,"hospital_name": "Zydus"},{"hospital_id": 6,"hospital_name": "Sterling" },{ "hospital_id": 7,"hospital_name": "KD"},{"hospital_id": 8, "hospital_name": "Kiran"},{"hospital_id": 9, "hospital_name": "Masum" },{ "hospital_id": 10, "hospital_name": "Solar"},{"hospital_id": 11,"hospital_name": "Apollo"},{"hospital_id": 12,"hospital_name": "Shalby"},{ "hospital_id": 13,"hospital_name": "Vaghasia Trust"}]
+
 
 def test_doctor_app():
     url = "https://patient-managment-api.herokuapp.com/hospital"
@@ -42,8 +43,8 @@ def test_doctor_app():
         'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NCwibmFtZSI6Im1lZXQiLCJ1c2VybmFtZSI6Im1lZXQyNzEyIiwiZW1haWwiOiJtZWV0LmJkYTE3MjlAaWN0LmdudS5hYy5pbiIsInVzZXJ0eXBlIjoiYWRtaW4iLCJwYXNzd29yZF9oYXNoIjoiJDJiJDEyJDNLZHpXZHF6RUN6TlU5RmRmWmVwM3VIcmhXajUuU3dpdjFNaHVRTWlMcVJaTFJHcUVaMnVDIn0.gKzJs32Z-a3SS-k5qZ-hVyhiyKoWXQKauyXHRojDzG8'
     }
 
-   # response = pytest_python_requests.request("GET", url, headers=headers, data=payload)
-    response =  pytest_python_requests.format_response("GET", url, headers=headers, data=payload)
+    response = pytest_python_requests.request("GET", url, headers=headers, data=payload)
+    #response =  pytest_python_requests.format_response("GET", url, headers=headers, data=payload)
 
     print(response.status_code)
     assert response.status_code == 200
